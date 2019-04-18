44ad77f7b14be8dbe859ada3c0548599859bdc6c
Lele Lew <lele@lelelew.com>
2019-04-17 22:59:33 -0700

    added read only mode for entry

--- src/EntryForm.js
+++ src/EntryForm.js
@@ -8,6 +8,8 @@ import ErrorIcon from "@material-ui/icons/Error";
 import CloseIcon from "@material-ui/icons/Close";
 import SnackbarContent from "@material-ui/core/SnackbarContent";
 import { Snackbar } from "@material-ui/core";
+import FormControlLabel from "@material-ui/core/FormControlLabel";
+import Switch from "@material-ui/core/Switch";
 
 const styles = theme => ({
   button: {
@@ -45,7 +47,8 @@ const defaultState = {
     goals: "",
     wins: "",
     lessonsLearned: ""
-  }
+  },
+  mode: "readOnly"
 };
 
 class EntryForm extends Component {
@@ -59,6 +62,7 @@ class EntryForm extends Component {
     if (props.selectedEntry) {
       this.state.entry = props.selectedEntry;
     }
+    this.onChangeMode = this.onChangeMode.bind(this);
   }
 
   async onSubmit(event) {
@@ -80,6 +84,10 @@ class EntryForm extends Component {
     this.setState({ hasError: false });
   }
 
+  onChangeMode(event) {
+    this.setState({ mode: event.target.checked ? "edit" : "readOnly" });
+  }
+
   handleChange(event) {
     const name = event.target.name;
     const value = event.target.value;
@@ -90,90 +98,121 @@ class EntryForm extends Component {
 
   render() {
     const { classes } = this.props;
-    const { entry, isSubmitting, hasError } = this.state;
+    const { entry, isSubmitting, hasError, mode } = this.state;
+    let content;
+    if (mode === "edit") {
+      content = (
+        <div>
+          <TextField
+            name="goals"
+            id="filled-multiline-flexible"
+            label="Goals"
+            multiline
+            rowsMax="4"
+            value={entry.goals || ""}
+            onChange={this.handleChange}
+            className={classes.textField}
+            margin="normal"
+            helperText="put your goals here"
+            variant="filled"
+          />
+
+          <TextField
+            name="wins"
+            id="filled-multiline-flexible"
+            label="Wins"
+            multiline
+            rowsMax="4"
+            value={entry.wins || ""}
+            onChange={this.handleChange}
+            className={classes.textField}
+            margin="normal"
+            helperText="put your wins here"
+            variant="filled"
+          />
+
+          <TextField
+            name="lessonsLearned"
+            id="filled-multiline-flexible"
+            label="Lessons Learned"
+            multiline
+            rowsMax="4"
+            value={entry.lessonsLearned || ""}
+            onChange={this.handleChange}
+            className={classes.textField}
+            margin="normal"
+            helperText="put your lessons learned here"
+            variant="filled"
+          />
+
+          <Button
+            variant="contained"
+            color="primary"
+            className={classes.button}
+            onClick={this.onSubmit}
+            disabled={isSubmitting}
+          >
+            Submit
+          </Button>
+
+          <Snackbar
+            open={hasError}
+            autoHideDuration={6000}
+            onClose={this.onCloseError}
+          >
+            <SnackbarContent
+              className={classes.error}
+              aria-describedby="client-snackbar"
+              message={
+                <span id="client-snackbar" className={classes.message}>
+                  <ErrorIcon className={classes.errorIcon} />
+                  Error saving entry. Try again later.
+                </span>
+              }
+              action={[
+                <IconButton
+                  key="close"
+                  aria-label="Close"
+                  color="inherit"
+                  className={classes.close}
+                  onClick={this.onCloseError}
+                >
+                  <CloseIcon className={classes.icon} />
+                </IconButton>
+              ]}
+            />
+          </Snackbar>
+        </div>
+      );
+    } else {
+      content = (
+        <div>
+          <h3>reading your journal</h3>
+          <strong>goals</strong>
+          <p>{entry.goals}</p>
+          <strong>wins</strong>
+          <p>{entry.wins}</p>
+          <strong>lesson learned</strong>
+          <p>{entry.lessonsLearned}</p>
+        </div>
+      );
+    }
 
     return (
       <div>
-        <h3>Daily</h3>
-        <TextField
-          name="goals"
-          id="filled-multiline-flexible"
-          label="Goals"
-          multiline
-          rowsMax="4"
-          value={entry.goals || ""}
-          onChange={this.handleChange}
-          className={classes.textField}
-          margin="normal"
-          helperText="put your goals here"
-          variant="filled"
+        <h3>daily</h3>
+        <FormControlLabel
+          control={
+            <Switch
+              checked={mode === "edit"}
+              onChange={this.onChangeMode}
+              value="checkedB"
+              color="primary"
+            />
+          }
+          label={mode === "edit" ? "editing" : "click to edit"}
         />
-
-        <TextField
-          name="wins"
-          id="filled-multiline-flexible"
-          label="Wins"
-          multiline
-          rowsMax="4"
-          value={entry.wins || ""}
-          onChange={this.handleChange}
-          className={classes.textField}
-          margin="normal"
-          helperText="put your wins here"
-          variant="filled"
-        />
-
-        <TextField
-          name="lessonsLearned"
-          id="filled-multiline-flexible"
-          label="Lessons Learned"
-          multiline
-          rowsMax="4"
-          value={entry.lessonsLearned || ""}
-          onChange={this.handleChange}
-          className={classes.textField}
-          margin="normal"
-          helperText="put your lessons learned here"
-          variant="filled"
-        />
-
-        <Button
-          variant="contained"
-          color="primary"
-          className={classes.button}
-          onClick={this.onSubmit}
-          disabled={isSubmitting}
-        >
-          Submit
-        </Button>
-
-        <Snackbar
-          open={hasError}
-          autoHideDuration={6000}
-          onClose={this.onCloseError}
-        >
-          <SnackbarContent
-            className={classes.error}
-            aria-describedby="client-snackbar"
-            message={
-              <span id="client-snackbar" className={classes.message}>
-                <ErrorIcon className={classes.errorIcon} />
-                Error saving entry. Try again later.
-              </span>
-            }
-            action={[
-              <IconButton
-                key="close"
-                aria-label="Close"
-                color="inherit"
-                className={classes.close}
-                onClick={this.onCloseError}
-              >
-                <CloseIcon className={classes.icon} />
-              </IconButton>
-            ]}
-          />
-        </Snackbar>
+        {content}
       </div>
     );
   }
